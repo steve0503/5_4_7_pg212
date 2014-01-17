c5421e736e874eca4e45c635b6b796644468d70f
steve0503 <leehr76@gmail.com>
2014-01-17 13:52:07 +0900

    Initial Commit

---
+++ 5_4_7/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  5_4_7
+//
+//  Created by SDT-1 on 2014. 1. 17..
+//  Copyright (c) 2014년 steve. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
